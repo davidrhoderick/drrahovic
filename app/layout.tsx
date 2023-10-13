8486911ca434e0657643ad6af3b46f8d06572bf1
David Rhoderick <david@vrio.com>
2023-10-14 00:16:24 +0200

    Differentiating development branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Dr. Rahovic',
+  title: 'Dr. Rahovic - Development',
   description: 'Ultrazuk',
 }
 
