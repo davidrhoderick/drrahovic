8e7183e0cea97940ee538e8e16faa0a4ca28f104
David Rhoderick <david@vrio.com>
2023-10-14 00:13:47 +0200

    Updating working hours.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="text-lg mb-2">Uskoro...</h1>
       <h2 className="text-2xl">Dr. Rahović</h2>
       <h3 className="text-xl mb-2">Ordinacija za ultrazvučnu dijagnostiku</h3>
-      <h4>Radno vrijeme - 8:00-15:30</h4>
+      <h4>Radno vrijeme - Po-Su - 8:00-15:30</h4>
     </main>
   )
 }
