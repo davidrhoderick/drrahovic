e5e5dd09bf61f1233d47e591181aa24124bb2d11
David Rhoderick <david@vrio.com>
2023-10-15 22:54:16 +0200

    Mobile responsiveness.

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,25 @@ import {
 export default function Home() {
   return (
     <main>
-      <section className="bg-mediumTeal grid grid-cols-2 p-10 gap-10 items-center">
-        <div className="bg-[url('/elen-sher-nbU4xF3YUbw-unsplash.jpg')] bg-cover bg-no-repeat aspect-[2/3] rounded-lg"></div>
-
-        <div className="flex flex-col">
-          <h1 className="font-headings text-white text-7xl text-center leading-tight">
+      <section className="bg-mediumTeal md:grid md:grid-cols-2 p-10 md:gap-10 md:items-center">
+        <div className="flex flex-col md:order-1">
+          <h1 className="font-headings text-white text-4xl md:text-7xl text-center leading-tight">
             Ordinacija za ultrazvučnu dijagnostiku Dr Rahović
           </h1>
 
           <a
             href="#zakazivanje-informacije"
-            className="font-body text-white inline-block p-4 bg-darkTeal rounded-lg font-bold mx-auto my-10"
+            className="font-body text-white inline-block p-4 bg-darkTeal rounded-lg font-bold mx-auto my-5 md:my-10"
           >
             Zakazivanje i informacije
           </a>
         </div>
+
+        <div className="bg-[url('/elen-sher-nbU4xF3YUbw-unsplash.jpg')] bg-cover bg-no-repeat aspect-[2/3] rounded-lg md:order-0"></div>
       </section>
 
-      <section className="grid grid-cols-3 p-10 pr-0 gap-10 items-center">
-        <div className="flex flex-col col-span-2">
+      <section className="md:grid md:grid-cols-3 p-10 md:pr-0 md:gap-10 md:items-center">
+        <div className="flex flex-col md:col-span-2">
           <h2 className="font-headings text-darkTeal text-6xl text-center leading-tight mb-5">
             Vršimo sledeće preglede
           </h2>
@@ -48,14 +48,14 @@ export default function Home() {
           </ul>
         </div>
 
-        <div className="bg-[url('/elen-sher-qrTdlfGDRdI-unsplash.jpg')] bg-cover bg-no-repeat aspect-[2/3] rounded-l-lg"></div>
+        <div className="bg-[url('/elen-sher-qrTdlfGDRdI-unsplash.jpg')] bg-cover bg-no-repeat aspect-[2/3] rounded-lg md:rounded-l-lg"></div>
       </section>
 
       <section className="bg-lightTeal pt-10" id="zakazivanje-informacije">
         <h2 className="text-center font-headings text-6xl mb-10">Kontakt</h2>
 
-        <div className="flex p-10 justify-center flex-wrap">
-          <div className="flex items-center w-1/2 mb-20 justify-center">
+        <div className="flex p-10 md:justify-center flex-wrap">
+          <div className="flex items-center md:w-1/2 mb-20 justify-center">
             <CalendarDaysIcon className="text-darkTeal h-10" />
 
             <div className="ml-4">
@@ -64,7 +64,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex items-center w-1/2 mb-20 justify-center">
+          <div className="flex items-center md:w-1/2 mb-20 justify-center">
             <PhoneIcon className="text-darkTeal h-10" />
 
             <div className="ml-4">
@@ -79,7 +79,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex items-center w-1/2 mb-10 justify-center">
+          <div className="flex items-center md:w-1/2 mb-10 justify-center">
             <MapPinIcon className="text-darkTeal h-10" />
 
             <div className="ml-4">
